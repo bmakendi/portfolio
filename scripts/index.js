4edeed63749081d6fa1e65e20b4b6cd9f50d3166
Bryan Makendi <bryan.makendi@gmail.com>
2022-02-10 02:07:47 +0100

    added assets and starting mobile menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,10 @@
+// handling menu button on click and when using key enter
 const btn = document.getElementById('menu-btn');
+
 btn.addEventListener('click', () => {
   btn.classList.toggle('active');
+});
+
+btn.addEventListener('keyup', e => {
+  if (e.key === 'Enter') btn.click();
 });
