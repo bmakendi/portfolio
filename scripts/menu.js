3a195206c87a44979f14135b08f503ff9cd25727
Bryan Makendi <bryan.makendi@gmail.com>
2022-02-13 14:15:40 +0100

    started works section

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,10 +4,14 @@ const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 const headerBtn = document.getElementById('headerBtn');
 const links = document.getElementsByClassName('link');
+const header = document.querySelector('header');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('active');
   menu.classList.toggle('active');
+  if (menu.classList.contains('active'))
+    header.style.backgroundColor = 'transparent';
+  else header.style.backgroundColor = '#272727';
 });
 
 btn.addEventListener('keyup', e => {
@@ -17,11 +21,17 @@ btn.addEventListener('keyup', e => {
 headerBtn.addEventListener('click', () => {
   btn.classList.remove('active');
   menu.classList.remove('active');
+  if (menu.classList.contains('active'))
+    header.style.backgroundColor = 'transparent';
+  else header.style.backgroundColor = '#272727';
 });
 
 for (link of links) {
   link.addEventListener('click', () => {
     btn.classList.remove('active');
     menu.classList.remove('active');
+    if (menu.classList.contains('active'))
+      header.style.backgroundColor = 'transparent';
+    else header.style.backgroundColor = '#272727';
   });
 }
