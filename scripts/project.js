68f11dd3cc7539899d45a0f6b6288000a84598ec
Bryan Makendi <bryan.makendi@gmail.com>
2022-02-26 23:28:32 +0100

    Dynamic integration of projects

--- scripts/project.js
+++ scripts/project.js
@@ -7,6 +7,10 @@ const main = () => {
   const project = getProject(param);
   console.log(project);
   title(project.name);
+  githubLink(project.githubLink);
+  websiteLink(project.webLink);
+  technosList(project.technos);
+  projectDescription(project.description);
 };
 
 const getProject = id => {
@@ -20,6 +24,50 @@ const getProject = id => {
 const title = projectName => {
   const h1 = document.getElementById('project-title');
   h1.innerText = projectName;
+};
+
+const githubLink = projectGithub => {
+  const ghLink = document.getElementById('github-link');
+  ghLink.setAttribute('href', projectGithub);
+};
+
+const websiteLink = projectWebsite => {
+  if (projectWebsite === '') return;
+  const linksContainer = document.getElementById('project-links');
+  const webLinkContainer = document.createElement('p');
+  const webLink = document.createElement('a');
+
+  webLinkContainer.classList.add('project__link', 'project__link--website');
+  webLink.setAttribute('href', projectWebsite);
+  webLink.setAttribute('target', 'blank');
+  webLink.setAttribute('rel', 'noopener noreferrer');
+  webLink.innerText = 'Website link';
+
+  webLinkContainer.appendChild(webLink);
+  linksContainer.appendChild(webLinkContainer);
+};
+
+const technosList = technos => {
+  const listContainer = document.getElementById('technos');
+
+  for (const techno of technos) {
+    const listItem = document.createElement('li');
+    const itemChip = document.createElement('img');
+
+    listItem.classList.add('project__techno');
+    itemChip.setAttribute('src', 'assets/design/chip.png');
+    itemChip.setAttribute('alt', 'Gradient decoration');
+    itemChip.classList.add('project__techno--chip');
+
+    listItem.appendChild(itemChip);
+    listItem.insertAdjacentText('beforeend', techno);
+    listContainer.appendChild(listItem);
+  }
+};
+
+const projectDescription = description => {
+  const descriptionContainer = document.getElementById('project-description');
+  descriptionContainer.innerText = description;
 };
 
 main();
